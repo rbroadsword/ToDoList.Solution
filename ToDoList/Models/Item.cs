5cf2974d9ed447989982341db9ab2cbe8a08dd9d
Ryan Broadsword <rbroadsword@gmail.com>
2022-06-26 16:37:57 -0700

    cleanup item.cs

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -4,25 +4,6 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    private string _description;
-    private string _add;
-    private string _view;
-
-    public Item(string description, string add, string view)
-    {
-      _description = description;
-      _add = add;
-      _view = view;
-    }
-
-    public string GetDescription()
-  {
-    return _description;
-  }
-    /*public bool IsString(string _description)
-    {
-      return (_add = typeof(string));
-    }*/ 
     public static void ClearAll()
     {
       _instances.Clear(); 
